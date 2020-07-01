2102111d664367facf13f7ae4b86c09a3712e7ff
kikirimha6 <67612801+kikirimha6@users.noreply.github.com>
2020-07-01 18:24:47 +0700

    Update ompong.php

--- ompong.php
+++ ompong.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set('Asia/Jakarta');
+date_default_timezone_set('Asia/Surabaya');
 include "function.php";
 echo color("green","💵              Auto Regist-Claim              💵\n");
 echo color("green","💵                Author : OMPONG                💵\n");
